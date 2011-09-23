5716b12303ebc8d1ef487422af0aff5d517c1c6d
Will Jessop <will@willj.net>
2011-09-23 15:31:33 +0200

    s/nick/user/, Just one last time.

--- lib/scamp/matcher.rb
+++ lib/scamp/matcher.rb
@@ -68,7 +68,7 @@ class Scamp
     def conditions_satisfied_by(msg)
       bot.logger.debug "Checking message against #{conditions.inspect}"
       
-      # item will be :nick or :room
+      # item will be :user or :room
       # cond is the int or string value.
       conditions.each do |item, cond|
         bot.logger.debug "Checking #{item} against #{cond}"
