8e17bc65a0dd8fcade961ef9adc1e688b35cb594
Mike Heffner <mikeh@fesnel.com>
2011-09-30 15:26:35 -0400

    If a block is passed to connect!, treat it as a post connection callback.
    This allows setting up a callback after the room list is populated
    (and more importantly the EM reactor has started). This can be used to
    join rooms after startup.

--- lib/scamp/connection.rb
+++ lib/scamp/connection.rb
@@ -2,7 +2,7 @@ class Scamp
   module Connection
     private
     
-    def connect(api_key, room_list)
+    def connect(api_key, room_list, &blk)
       EventMachine.run do
         
         # Check for rooms to join, and join them
@@ -15,6 +15,11 @@ class Scamp
         populate_room_list do
           logger.debug "Adding #{room_list.join ', '} to list of rooms to join"
           @rooms_to_join = room_list.map{|c| room_id(c) }
+
+          # Call a post connection block
+          if block_given?
+            yield
+          end
         end
         
         # populate bot data separately, in case we are ignoring ourselves
