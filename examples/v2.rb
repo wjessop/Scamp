a092fe74c9406cdab26de3e532cbe206a39dd18d
Adam Holt <me@adamholt.co.uk>
2012-11-21 15:54:09 +0000

    removed old example, cleaned up new example

--- examples/v2.rb
+++ examples/v2.rb
@@ -9,25 +9,7 @@ class PingPlugin < Scamp::Plugin
   match /^ping/, :say_pong
 
   def say_pong channel, msg
-    puts matches.inspect
     channel.say "pong"
-  end
-end
-
-class CommandListPlugin < Scamp::Plugin
-  match "help", :display_command_list
-
-  def display_command_list context, msg
-    max_command_length = command_list.map{|cl| cl.first.to_s }.max_by(&:size).size
-    max_adapter_length = command_list.map{|cl| cl[1].join(",").to_s }.max_by(&:size).size
-    command_format_string = "%#{max_command_length + 1}s"
-    adapter_format_string = "%-#{max_adapter_length + 1}s"
-    formatted_commands = command_list.map{|action, adapters, conds| "#{sprintf(command_format_string, action)} | #{sprintf(adapter_format_string, adapters.join(","))} | #{conds.size == 0 ? '' : conds.inspect}"}
-    context.say <<-EOS
-#{sprintf("%-#{max_command_length + 1}s", "Command match")} | #{sprintf(adapter_format_string, "Adapters")} | Conditions
---------------------------------------------------------------------------------
-#{formatted_commands.join("\n")}
-    EOS
   end
 end
 
@@ -40,7 +22,7 @@ class TestAdapter < Scamp::Adapter
 
   def connect!
     EventMachine::PeriodicTimer.new(@opts[:delay]) do
-      msg = Scamp::Message.new(self, :body => "help")
+      msg = Scamp::Message.new(self, :body => "ping")
       context = TestAdapter::Context.new
       push [context, msg]
     end
@@ -52,7 +34,6 @@ Scamp.new do |bot|
   bot.adapter :another, TestAdapter, :delay => 5
 
   bot.plugin PingPlugin, :on => [:test]
-  bot.plugin CommandListPlugin
 
   bot.connect!
 end
